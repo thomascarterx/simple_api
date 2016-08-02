3857c53a2c268a6fac696da4352a2f5fc3933232
Thomas Carter <thomascarterx@gmail.com>
2016-08-02 00:08:19 -0400

    Sets the route constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Api definition
+  # 
+  # To generate a base_uri under a subdomain, e.g. api.simple_api.dev,
+  # you would want to do the following:
+  # 
+  # namespace :api, defaults: { format: :json },
+  #                            constraints: { subdomain: 'api' }, path: '/'  do
+  #   scope module: :v1,
+  #             constraints: ApiConstraints.new(version: 1, default: true) do
+  #     # We are going to list our resources here
+  #   end
+  # end
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1 do
+        # Resources
+    end
+  end
 end
